bb82082753012c5e6624b1bc65a435ef86c49017
gordonki <pribytkingorde@gmail.com>
2019-10-26 03:33:58 +0300

    Map class for managing the map (adjacency list).

--- map_class.py
+++ map_class.py
@@ -1,3 +1,10 @@
+''''
+    Todo:
+    Procedural vertex naming
+    ...
+'''
+
+
 class Map:
     adjacency_dict = {}
     vertex_coordinates_dict = {}
@@ -5,19 +12,12 @@ class Map:
 
     def addVertex(self, name, coords):
         current_vertecies = self.current_vertecies
-        vertex_coordinates_dict = self.vertex_coordinates_dict
-        current_vertecies.append(name)
         if name not in current_vertecies:
-            vertex_coordinates_dict[name] = coords
+            self.vertex_coordinates_dict[name] = coords
             self.adjacency_dict[name] = []
+            current_vertecies.append(name)
 
     def addRoad(self, origin, destination, time, two_way):
-         self.adjacency_dict[origin] = [destination, time]
-         if two_way:
-             self.adjacency_dict[destination] = [origin, time]
-
-
-
-
-
-
+        self.adjacency_dict[origin] = [destination, time]
+        if two_way:
+            self.adjacency_dict[destination] = [origin, time]
