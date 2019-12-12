b64fad438edc1991a2468f2556e3a7639d550138
Maxim Makarskij <makarskij.m@yandex.ru>
2019-12-12 22:45:17 +0200

    draw line from clicks

--- display.py
+++ display.py
@@ -1,8 +1,8 @@
 from tkinter import *
 import map_class as mp
-
 import math
 from PIL import Image, ImageTk
+
 
 ''' connected with map creating '''
 
@@ -22,20 +22,51 @@ def creating_map():
     creating_nodes(coordinates)
 
 
-def creating_path(path, line=None):
+def creating_path(path, start, finish, line=None):
     c.delete("all")
     create_background_picture()
     creating_map()
     c.tag_lower(img)
-    print(path)
-    for i in range(len(path[0])-1):
+    for i in range(len(path[0]) - 1):
         node_name = path[0][i]
-        next_node_name = path[0][i+1]
+        next_node_name = path[0][i + 1]
         xStart, yStart = int(name_to_coordinates_dict[node_name][0]), int(name_to_coordinates_dict[node_name][1])
         xFinish, yFinish = int(name_to_coordinates_dict[next_node_name][0]), int(name_to_coordinates_dict[next_node_name][1])
-        print(xStart, yStart, xFinish, xFinish)
-        line = c.create_line(xStart, yStart, xFinish, yFinish, width=3, fill="#6f2da8")
-        c.tag_raise(line)
+        if i == len(path[0]) - 2 and i != 0:
+            x4, y4 = p4((xStart, yStart), (xFinish, yFinish), (finish[0], finish[1]))
+            line1 = c.create_line(finish[0], finish[1], x4, y4, width=3, fill="#6f2da8")
+            line = c.create_line(x4, y4, xStart, yStart, width=3, fill="#6f2da8")
+            c.tag_raise(line)
+            c.tag_raise(line1)
+        if i == 0 and i != len(path[0]) - 2:
+            x4, y4 = p4((xStart, yStart), (xFinish, yFinish), (start[0], start[1]))
+            line1 = c.create_line(start[0], start[1], x4, y4, width=3, fill="#6f2da8")
+            line = c.create_line(x4, y4, xFinish, yFinish, width=3, fill="#6f2da8")
+            c.tag_raise(line)
+            c.tag_raise(line1)
+        if len(path[0]) - 2 == 0:
+            x4, y4 = p4((xStart, yStart), (xFinish, yFinish), (finish[0], finish[1]))
+            line = c.create_line(finish[0], finish[1], x4, y4, width=3, fill="#6f2da8")
+            x3, y3 = p4((xStart, yStart), (xFinish, yFinish), (start[0], start[1]))
+            line1 = c.create_line(start[0], start[1], x3, y3, width=3, fill="#6f2da8")
+            line2 = c.create_line(x4, y4, x3, y3, width=3, fill="#6f2da8")
+            c.tag_raise(line)
+            c.tag_raise(line1)
+            c.tag_raise(line2)
+        if not (i == len(path[0]) - 2 or i == 0 or len(path[0] - 2) == 0):
+            line = c.create_line(xStart, yStart, xFinish, yFinish, width=3, fill="#6f2da8")
+            c.tag_raise(line)
+
+
+def p4(p1, p2, p3):
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+    dx, dy = x2 - x1, y2 - y1
+    det = dx * dx + dy * dy
+    a = (dy * (y3 - y1) + dx * (x3 - x1)) / det
+    return x1 + a * dx, y1 + a * dy
+
 
 def creating_nodes(coord):
     for Node in coord:
@@ -66,11 +97,15 @@ def e():
 
 
 def closest_node_name(event):
-    global from_node_name, to_node_name, path, counter
+    global from_node_name, to_node_name, path, counter, finish, start
     if not counter == 2:
         counter += 1
         x = event.x
         y = event.y
+        if counter == 1:
+            start = [x, y]
+        else:
+            finish = [x, y]
         x_closest = None
         y_closest = None
         length = None
@@ -91,14 +126,17 @@ def closest_node_name(event):
         print(from_node_name, to_node_name, event)
         if to_node_name is not None:
             path = mp.Map().findShortestPath(from_node_name, to_node_name)
-            creating_path(path)
+            creating_path(path, start, finish)
             creating_nodes(coordinates)
     print(counter)
+
 
 # statics & variables
 
 
 counter = 0
+finish = None
+start = None
 from_node_name, path, to_node_name, image = None, None, None, None
 mp.Map().decodeMap("map.txt")
 coordinates = mp.Map().current_vertex_coords
